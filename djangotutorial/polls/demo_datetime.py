0dc3f9d318e0481535942d1272005bbeafb457c6
Anh Van <anhvandinh0@gmail.com>
2025-08-26 17:31:05 +0700

    Apply .gitignore

--- djangotutorial/polls/demo_datetime.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from django.utils import timezone
-from models import Question
-# Tạo một Question với pub_date 30 ngày trong tương lai
-future_question = Question(pub_date=timezone.now() + datetime.timedelta(days=30))
-
-# Kiểm tra xem nó có được xuất bản gần đây không
-print(future_question.was_published_recently())
