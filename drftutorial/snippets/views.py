72630ff920a1f1dda1a3fe9688199364f1a0cdae
Anh Van <anhvandinh0@gmail.com>
2025-09-16 14:04:10 +0700

    Relationships & Hyperlinked APIs

--- drftutorial/snippets/views.py
+++ drftutorial/snippets/views.py
@@ -9,6 +9,12 @@ from snippets.serializers import UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework import generics
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -36,8 +42,19 @@ class SnippetList(APIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class SnippetDetail(APIView):
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
     
+
+class SnippetDetail(APIView):
+
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_object(self, pk):
@@ -63,3 +80,11 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
