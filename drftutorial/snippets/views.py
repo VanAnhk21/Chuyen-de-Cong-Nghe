3a4b498e998acb8c37ece9ece6b9d66cf098bdf6
Anh Van <anhvandinh0@gmail.com>
2025-09-16 13:54:49 +0700

    Authentication & Permissions

--- drftutorial/snippets/views.py
+++ drftutorial/snippets/views.py
@@ -4,11 +4,24 @@ from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from django.http import Http404
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class SnippetList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -20,12 +33,13 @@ class SnippetList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Snippet.objects.get(pk=pk)
